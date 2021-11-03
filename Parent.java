4c5cb336a32d1914f631ef18c15f7255c812b8b3
Robeil Aregawi <robeilaregawi1@gmail.com>
2021-11-03 16:51:46 -0500

    starting lab_5 polymorphism

---
+++ Parent.java
@@ -0,0 +1,27 @@
+package CS203Course.Lab_5;
+
+/*class Parent {
+
+    void myMethod() {
+        System.out.println("Super class");
+    }
+}
+
+class Derived extends Parent{
+
+    void myMethod(){
+        System.out.println("Derived class");
+    }
+
+    public static void main(String[] args) {
+
+        Derived object = new Parent();
+
+        object.myMethod();
+    }
+}*/
+
+/**
+ * The error will be fixme => compilation error
+ *
+ */
